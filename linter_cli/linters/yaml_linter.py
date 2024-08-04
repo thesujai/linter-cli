10e439d43f0b614d5af9a66b514e90669d06045f
Sujai Kumar Gupta <b210031@nitsikkim.ac.in>
2024-08-04 17:13:12 +0530

    use click.echo instead of print and define result as none before to avoid reference error

--- linter_cli/linters/yaml_linter.py
+++ linter_cli/linters/yaml_linter.py
@@ -1,14 +1,16 @@
 import subprocess
+import click
 
 
 def lint_yaml(file_path):
+    result = None
     try:
         result = subprocess.run(["yamllint", file_path], capture_output=True, text=True)
         if result.returncode == 0:
-            print(f"{file_path} is valid YAML")
+            click.echo(f"{file_path} is valid YAML")
         else:
             print(result.stdout)
     except Exception as e:
-        print(f"Error linting {file_path}: {e}")
+        click.echo(f"Error linting {file_path}: {e}")
 
     return result
